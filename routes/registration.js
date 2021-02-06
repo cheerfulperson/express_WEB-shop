b752f0e135143668dc70744b86876a759da3a019
cheerfulperson <43146047+cheerfulperson@users.noreply.github.com>
2021-02-06 20:35:11 +0300

    init: rus-leng and change reg....

--- routes/registration.js
+++ routes/registration.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const db = require('../moduls/DB-config');
-const crypto = require('crypto')
+const crypto = require('crypto');
+
 const router = express.Router();
-
-
 
 /* GET registration listing. */
 router.get('/', function (req, res, next) {
@@ -34,7 +33,7 @@ router.post('/', function (req, res, next) {
   }
 
   let selectSQL = `SELECT email FROM mytable WHERE (email=?)`;
-  let insertUserSQL = `INSERT INTO mytable(email, password, name, personalID,type_user) VALUES (?,?,?,?,?)`;
+  let insertUserSQL = `INSERT INTO mytable(email, password, name, personalID, type_user) VALUES (?,?,?,?,?)`;
   let hbsOptionsObject = {
     isBottonHeader: false,
     isEmailsReapet: true,
@@ -46,23 +45,21 @@ router.post('/', function (req, res, next) {
     pwsReapet: req.body.passwordRepeat
   };
 
-// Добавить пользователя. С*ка вот здесь он не работает, а теперь работает
   //Проверка на схожесть паролей
   if (user.password == req.body.passwordRepeat && user.password.length >= 6) {
     // Проверка 
     db.query(selectSQL, [user.email, user.name], function (error, results) {
       if (error) console.log(error);
-
       if (results.length === 0) {
 
-        db.query('SELECT * FROM mytable', (err, results) => {
 
           db.query(insertUserSQL, [user.email, user.password, user.name, user.personalID, user.type_user], (error, results) => {
             if (error) console.log(error);
             else res.redirect('/login');
-            db.end();
-          }); 
-        });
+          });
+
+        // Добавить пользователя. С*ка вот здесь он не работает, а теперь работает
+
       } else {
         res.render('layouts/registration', hbsOptionsObject);
       }
